ba1dd0d592ecab384379d008e41087631c8a2a29
hmruhul <hmruhul16.mbstu@gmail.com>
2023-09-18 14:27:53 +0600

    Lab

---
+++ SQL - 45/1084. Sales Analysis III.sql
@@ -0,0 +1,8 @@
+select product_id, product_name
+from product
+where  product_id in (
+  select product_id from sales
+)
+and  product_id not in(
+  select product_id from sales where  sale_date < '2019-01-01' or sale_date > '2019-03-31'
+)
