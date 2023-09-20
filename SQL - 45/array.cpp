3500e8489fbbf5bf8fc0ea272c796d00882a6406
hmruhul <hmruhul16.mbstu@gmail.com>
2023-09-20 15:39:43 +0600

    array

--- SQL - 45/array.cpp
+++ SQL - 45/array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+arrpush() {
     int arr[] = {10, 12, 13, 14, 15};
     int index = 2, value = 20;
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -13,5 +13,31 @@ int main() {
         cout << arr[i] << " ";
     }
     cout << endl;
-    return 0;
+}
+int main(){
+    //arrpush();
+    int arr[] = {10, 12, 13, 13, 14, 14, 15};
+    set<int>se;
+    int j = 0;
+    for(int i=0; i <sizeof(arr)/sizeof(arr[0]);){
+        if(se.find(arr[i]) != se.end()){
+            i++;
+        }
+        else {
+            arr[j] = arr[i];
+            j++, i++;
+        }
+        se.insert(arr[i-1]);
+    }
+    for(auto i:se) {
+        cout<<i<<" ";
+     
+    }
+    cout<<endl;
+    for(int i = 0; i<se.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
 }
